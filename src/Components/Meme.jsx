6e6ea71e443c17feac62b2c4880012297fab45a3
Vickopiyo <opiyookoth2016@gmail.com>
2023-01-04 22:25:06 +0300

    Feat: Gets memes successfully when fetched

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -1,49 +1,58 @@
-import React from 'react' 
-import data from "../Components/MemeData"
+import React, { useState } from 'react' 
+import memesData from "../Components/MemeData"
 
 const Meme = () => {      
     
     // Props should always be immutable       
-    // State(variables in a function)--values managed within a component which keeps changing    
-
-    // NAME--equivalent props
-      function greeting(name){ 
-//    TIMEOFDAY--equivalent to State  
-
-        const  date = new Date()
-        const time = date.getHours()     
-        let timeOfDay 
-        if(time < 12 ){
-            timeOfDay = "Good Morning"
-        }else if(time >=12 && time < 19 ){
-            timeOfDay = "Good Afternoon"
-        } else {
-            timeOfDay = "Good Evening"
-        }    
-
-        return `${timeOfDay} ${name} ` 
+    // State(variables in a function)--values managed within a component which keeps changing   
         
-      }      
+    //   const [count, setCount]= useState(0)     
 
+    // NEVER!! -- setCount( count++(count = count + 1) )--Reason ? it modifies state directly    
+    // BEST Practice for updating state is through callback func => setCount(prevCount => prevCount + 1)
+    // NAME--equivalent props
+    
+    //   function greeting(name){ 
+    // //TIMEOFDAY--equivalent to State  
+           
+    //     const  date = new Date()  
 
-      console.log(greeting("Vick"))
+    //     const time = date.getHours()     
 
+    //     let timeOfDay  
 
+    //     if(time < 12 ){
+    //         timeOfDay = "Good Morning"
+    //     }else if(time >=12 && time < 19 ){
+    //         timeOfDay = "Good Afternoon"
+    //     } else {
+    //         timeOfDay = "Good Evening"
+    //     }    
 
+    //     return `${timeOfDay} ${name} ` 
 
+    //   }      
+
+    //   console.log(greeting("Vick"))
 
 
 //     const memes = data.map( meme => {      
 //         return <Meme key={meme.id}  {...m
 //       }) 
-//    }       
+//    }         
+     const [meme, setMeme]= useState("")       
 
+function getMemeImage(){      
+    
+         const memesArray = memesData.data.memes 
+         const randomNumber = Math.floor(Math.random()*memesArray.length)    
+     
 
-function getMemeImage(){        
+        //  You dont previous state though so  setMeme(memesArray[randomNumber].url) works fine 
 
-    console.log("Clicked")
-}    
-  
+        setMeme(prevMeme =>  prevMeme = memesArray[randomNumber].url) 
+}        
+     
   return (    
 
     <section>
@@ -51,8 +60,13 @@ function getMemeImage(){
             <input type="text"   className='form-input'  placeholder='Top Text' />  
             <input type="text"   className='form-input'  placeholder='Bottom Text' /> 
             {/* No () because as soon as it renders it call the function.It should only work when clicked  */}
-            <button  onClick={getMemeImage} className='form-btn '>Generate Meme</button>  
+            <button  onClick={getMemeImage} className='form-btn '>Generate Meme</button> 
         </div>    
+        <div className="meme">  
+        <img  src={meme}   alt="meme!"  />   
+    
+        </div> 
+
     </section>    
   )
 }   
