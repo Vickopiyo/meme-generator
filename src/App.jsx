85d40c1b4fb0759d83433417402c32545c199737
Vickopiyo <opiyookoth2016@gmail.com>
2023-01-04 10:32:23 +0300

    Files and folders structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./Components/Header"
+
+function App() {
+
+  return (
+    <div className="App">   
+
+      <Header />
+        dghgdhhgdhgfdh
+    </div>
+  )
+}
+
+export default App
