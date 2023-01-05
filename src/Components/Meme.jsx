6ccadd72953a795f0bd61697088b7accf8cc9963
Vickopiyo <opiyookoth2016@gmail.com>
2023-01-05 23:46:16 +0300

    Add: updating complex state objects

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -30,7 +30,7 @@ const Meme = () => {
     //     }else if(time >=12 && time < 19 ){
     //         timeOfDay = "Good Afternoon"
     //     } else {  
-      
+
     //         timeOfDay = "Good Evening"
     //     }    
 
@@ -38,40 +38,100 @@ const Meme = () => {
 
     //   }      
 
-    //   console.log(greeting("Vick"))
+    //   console.log(greeting("Vick"))      
+             
+       
+    //     UPDATING AN ARRAY STATE ALWAYS USE THE SPREAD OPERATOR TO AVOID DIRECT MUTATION  
+
+    // .push mutates state directly which is against principle of state  
+
+    // const [thingsArray, setThingsArray] = React.useState(["Thing 1", "Thing 2"])
+    
+    // function addItem() {
+    //     // We'll work on this next
+    //     setThingsArray(prevThingsArray => {
+    //     return     [...prevThingsArray, `Thing ${prevThingsArray.length + 1}`]})   
+    // } 
+    
+    // const thingsElements = thingsArray.map(thing => <p key={thing}>{thing}</p>)
 
 
 //     const memes = data.map( meme => {      
 //         return <Meme key={meme.id}  {...m
 //       }) 
-//    }         
-     const [meme, setMeme]= useState("")       
+//    }            
 
-function getMemeImage(){      
+
+
+
+//   UPDATING  AN OBJECT STATE         
+
+// const [contact, setContact] = React.useState({
+//   firstName: "John",
+//   lastName: "Doe",
+//   phone: "+1 (719) 555-1212",
+//   email: "itsmyrealname@example.com",
+//   isFavorite: true
+// })         
+
+   
+// func triggered when  star icon in contact is clicked(changes fav to true or false)
+
+// function toogleFavorite(){   
+
+//    setContact( prevState =>   
+
+//       {...prevState, isFavorite : !prevState.isFavorite}  
+      
+//       )
+// }     
+   
+// colors  used on the icon  when icon    
+
+// let starIcon = isFavorite ? "yellow.png"  : "empty.png"
+
+// <img 
+// src={`../images/${starIcon}`} 
+// className="card--favorite"
+// onClick={toggleFavorite}
+// />
+
+
+
+
+
+
+
+ const [meme, setMeme]= useState("")       
+      
+
+function getMemeImage(){         
     
          const memesArray = memesData.data.memes 
-         const randomNumber = Math.floor(Math.random()*memesArray.length)    
-        //  You dont previous state though so  setMeme(memesArray[randomNumber].url) works fine 
+         const randomNumber = Math.floor(Math.random()*memesArray.length)      
+
+        //  You dont need to access previous state  so setMeme(memesArray[randomNumber].url) works fine 
 
         setMeme(   prevMeme =>  prevMeme = memesArray[randomNumber].url) 
 }            
 
-    //  callback in  setState is when there is need to access prevstate before updating.Common use is toggling(true to false or false to true) 
+    //  callback in setState is when there is need to access prevstate before updating.Common use is toggling(true to false or false to true) setState( prevState => !prevState )
             
   return (    
 
     <section>
-        <div  className='form'>    
+        <div  className='form'>                
+
             <input type="text"   className='form-input'  placeholder='Top Text' />  
             <input type="text"   className='form-input'  placeholder='Bottom Text' /> 
             {/* No () because as soon as it renders it call the function.It should only work when clicked  */}
             <button  onClick={getMemeImage} className='form-btn '>Generate Meme</button> 
         </div>    
-        <div className="meme">  
+        <div className="meme">                
         <img  src={meme}   alt="meme!"  />   
-        </div> 
+        </div>     
     </section>    
   )         
 }                   
 
-export default Meme                
+export default Meme                    
